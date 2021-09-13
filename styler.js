75fa7206465aa9de5ab151ff1f20ae6431c1e260
Moonlight404 <59587363+Moonlight404@users.noreply.github.com>
2021-09-13 17:28:54 -0300

    Fix

--- styler.js
+++ styler.js
@@ -224,7 +224,7 @@ class Styler {
                     }
                 })
             } catch (error) {
-                console.log(error)
+                //console.log(error)
             }
             if (loops >= document.querySelectorAll('*').length * 2) {
                 clearInterval(timer)
